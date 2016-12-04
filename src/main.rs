79864c70d24d7b26f267495266a37cfce4210a1a
Yale Cason <yale.cason@gmail.com>
2016-12-04 02:03:34 -0500

    day4

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
+#![feature(slice_concat_ext)]
 // mod day1;
 // mod day2;
-mod day3;
+// mod day3;
+mod day4;
 
 fn main() {
     // day1::main();
     // day2::main();
-    day3::main();
+    // day3::main();
+    day4::main();
 }
