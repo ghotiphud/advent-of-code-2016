f11c205fad96e28704268621d16300f02d68681d
Yale Cason <yale.cason@gmail.com>
2016-12-07 01:30:57 -0500

    day7

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 #![feature(slice_concat_ext)]
 #![feature(conservative_impl_trait)]
 
-#![allow(dead_code, unused_features)]
+#![allow(dead_code, unused_features, unused_variables)]
 
 mod day1;
 mod day2;
@@ -9,12 +9,14 @@ mod day3;
 mod day4;
 mod day5;
 mod day6;
+mod day7;
 
 fn main() {
     // day1::main();
     // day2::main();
     // day3::main();
     // day4::main();
-    day5::main();
+    // day5::main();
     // day6::main();
+    day7::main();
 }
